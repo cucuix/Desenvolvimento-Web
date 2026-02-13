02509a9f23ecfc44edaa92fec3214b708aa87406
Miguel <miguelxavier1901@gmail.com>
2026-02-12 21:27:50 -0300

    aula 12/02

--- minha-spa/src/pages/Sobre.jsx
+++ minha-spa/src/pages/Sobre.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Sobre.module.css'
 
-export function Sobre(){
+export function Sobre (){
     return(
         <div className={styles.container}>
             <header className={styles.header}>
@@ -14,11 +14,15 @@ export function Sobre(){
                 <div className={styles.card}>
                     <h3>Capacidades Técnicas</h3>
                     <ul className={styles.list}>
-                        
+                        <li>Estrutura do Framework</li>
+                        <li>Gerenciamento de Rotas</li>
+                        <li>Estilização por Componentes</li>
                     </ul>
                 </div>
             </section>
-
+            <footer className={styles.footer}>
+                <p>Desenvolvido durante o aprendizado de Front-End</p>
+            </footer>
         </div>
     )
 }
